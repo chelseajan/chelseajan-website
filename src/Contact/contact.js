b87346146056cafe4ddebad4e286818774d2b301
chelsealjan <chelsealjan@gmail.com>
2020-07-28 12:24:04 -0700

    setup contact component

---
+++ src/Contact/contact.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Title from '../Title/title';
+
+
+const work = (props) => {
+    return (
+    <div className="contact background-layer">
+        <div className="container section">
+            <Title id="Contact" title="Contact."/>
+            <div className="grid">
+                <div className="work-column">
+                  Write me an email. 
+                </div>
+                <div className="work-column">
+                  Or Social 
+                </div>
+                
+            </div>
+        </div>
+    </div>
+        
+    )
+}
+
+export default work
+
