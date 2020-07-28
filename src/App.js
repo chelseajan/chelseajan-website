b6b5701ed0f18b4d79a12c7ac8b2e6d938b52212
chelsealjan <chelsealjan@gmail.com>
2020-07-28 12:23:23 -0700

    updated app component with section comps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar/navbar';
+import Footer from './Footer/footer';
+import Banner from './Banner/banner';
+import About from './About/about';
+import Work from './Work/work';
+import Uses from './Uses/uses';
+import Contact from './Contact/contact';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Navbar name="Chelsea Jan" />
+      <Banner greeting="Hi there!" name="Chelsea" title="I currently work at Illumina, improving human health by unlocking the power of the genome." />
+      <About />
+      <Work />
+      <Uses />
+      <Contact />
+      {/* <Section id="Uses" title="Uses." />
+      <Section id="Contact" title="Contact." /> */}
+
+      <Footer />
     </div>
   );
 }
