19f5692340a2649d5f5b2b19aed3a76d6717b2f5
chelsealjan <chelsealjan@gmail.com>
2020-08-22 11:54:44 -0700

    updated contact

--- src/Contact/contact.js
+++ src/Contact/contact.js
@@ -3,18 +3,46 @@ import Title from '../Title/title';
 
 
 const work = (props) => {
+  const socialLinks = [
+    {
+        site: "LinkedIn",
+        href: "https://www.linkedin.com/in/chelseajan/"
+    },
+    {
+        site: "Github",
+        href: "https://github.com/chelseajan"
+    }
+];
     return (
     <div className="contact background-layer">
         <div className="container section">
             <Title id="Contact" title="Contact."/>
             <div className="grid">
+              <div className="work-column">
+              </div>
+
+              <div className="work-column">
+              </div>
+
                 <div className="work-column">
-                  Write me an email. 
+                  <h4>Write me an email. </h4>
+                  <a href = "mailto: chelsealjan@gmail.com">chelsealjan@gmail.com</a>
+
                 </div>
+
                 <div className="work-column">
-                  Or Social 
+                  <h4>Or Social Media, if that's your thing.</h4>
+                  <ul>
+                  {
+                    socialLinks.map (data => {
+                        return (
+                        <li><a className="social-link" href={data.href}>{data.site}</a></li>
+                        )
+                    })
+                  }
+                  </ul>
+
                 </div>
-                
             </div>
         </div>
     </div>
